404b4d35579dbef5eec3e8949dba43f57a746cfd
JS Fauteux <js.fauteux@unity3d.com>
2023-04-27 18:27:07 -0400

    Window title updated to relfect version and rev from core diff --git a/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/WindowController.cs b/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/WindowController.cs index 59e3b6b..7c7ee45 100644 --- a/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/WindowController.cs +++ b/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/WindowController.cs @@ -1,11 +1,13 @@  ﻿using ObjCRuntime; - +using RandomizerCore.Controllers;  using System;  namespace MinishCapRandomizerMacUI  {      [Register("WindowController")]      public class WindowController : NSWindowController      { +        ShufflerController ShufflerController = new ShufflerController(); +          protected WindowController(NativeHandle handle) : base(handle)          {
    @@ -15,7 +17,7 @@ namespace MinishCapRandomizerMacUI
             {
                 base.WindowDidLoad();
    
    -
    +            Window.Title = $"Minish Cap Randomizer {ShufflerController.VersionName} {ShufflerController.RevName}";
             }
         }
     }

--- MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/WindowController.cs
+++ MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/WindowController.cs
@@ -1,11 +1,13 @@
 ﻿using ObjCRuntime;
-
+using RandomizerCore.Controllers;
 using System;
 namespace MinishCapRandomizerMacUI
 {
     [Register("WindowController")]
     public class WindowController : NSWindowController
     {
+        ShufflerController ShufflerController = new ShufflerController();
+
         protected WindowController(NativeHandle handle) : base(handle)
         {
 
@@ -15,7 +17,7 @@ namespace MinishCapRandomizerMacUI
         {
             base.WindowDidLoad();
 
-
+            Window.Title = $"Minish Cap Randomizer {ShufflerController.VersionName} {ShufflerController.RevName}";
         }
     }
 }
