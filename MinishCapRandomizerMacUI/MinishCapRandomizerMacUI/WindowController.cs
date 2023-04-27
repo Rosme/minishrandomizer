9e56fcf91547f8ec11a8850ce88470beef4ea9e1
JS Fauteux <js.fauteux@unity3d.com>
2023-04-27 18:22:03 -0400

    Added window controller diff --git a/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/Main.storyboard b/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/Main.storyboard index 6dc2ada..083879b 100644 --- a/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/Main.storyboard +++ b/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/Main.storyboard @@ -15,7 +15,7 @@                                  <modifierMask key="keyEquivalentModifierMask"/>                                  <menu key="submenu" title="MinishCapRandomizerMacUI" systemMenu="apple" id="uQy-DD-JDr">                                      <items> -                                        <menuItem title="About MinishCapRandomizerMacUI" id="5kV-Vb-QxS"> +                                        <menuItem title="About MinishCap Randomizer" id="5kV-Vb-QxS">                                              <modifierMask key="keyEquivalentModifierMask"/>                                              <connections>                                                  <action selector="orderFrontStandardAboutPanel:" target="Ady-hI-5gd" id="Exp-CZ-Vem"/> @@ -103,7 +103,7 @@          <!--Window Controller-->          <scene sceneID="R2V-B0-nI4">              <objects> -                <windowController id="B8D-0N-5wS" sceneMemberID="viewController"> +                <windowController id="B8D-0N-5wS" customClass="WindowController" sceneMemberID="viewController">                      <window key="window" title="Window" allowsToolTipsWhenApplicationIsInactive="NO" autorecalculatesKeyViewLoop="NO" releasedWhenClosed="NO" animationBehavior="default" id="IQv-IB-iLA">                          <windowStyleMask key="styleMask" titled="YES" closable="YES" miniaturizable="YES" resizable="YES"/>                          <windowPositionMask key="initialPositionMask" leftStrut="YES" rightStrut="YES" topStrut="YES" bottomStrut="YES"/> diff --git a/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/WindowController.cs b/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/WindowController.cs new file mode 100644 index 0000000..59e3b6b --- /dev/null +++ b/MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/WindowController.cs @@ -0,0 +1,22 @@ +﻿using ObjCRuntime; + +using System; +namespace MinishCapRandomizerMacUI +{ +    [Register("WindowController")] +    public class WindowController : NSWindowController +    { +        protected WindowController(NativeHandle handle) : base(handle) +        { + +        } + +        public override void WindowDidLoad() +        { +            base.WindowDidLoad(); + + +        } +    } +} +

---
+++ MinishCapRandomizerMacUI/MinishCapRandomizerMacUI/WindowController.cs
@@ -0,0 +1,22 @@
+﻿using ObjCRuntime;
+
+using System;
+namespace MinishCapRandomizerMacUI
+{
+    [Register("WindowController")]
+    public class WindowController : NSWindowController
+    {
+        protected WindowController(NativeHandle handle) : base(handle)
+        {
+
+        }
+
+        public override void WindowDidLoad()
+        {
+            base.WindowDidLoad();
+
+
+        }
+    }
+}
+
